1f2e4fc75292406fd704e0e2b4783211cb1bbf66
realsgy <gyorgy@outlook.com>
2021-04-25 17:01:35 -0700

    use environment var for SDK dir

--- VRFPSLogger.cpp
+++ VRFPSLogger.cpp
@@ -106,23 +106,12 @@ int main(int argc, char* argv[])
 	std::thread loggerThread(log_fps, pFile);
 	std::cout << "Press any key to stop\n";
 	std::cin.get();
+	std::cout << "Exiting...\n";
 	_fStop = true;
 	loggerThread.join();
 	if (pFile) {
 		pFile->close();
 	}
-	std::cout << "Bye\n";
-	
 	vr::VR_Shutdown();
+	std::cout << "Bye\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
